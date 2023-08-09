91b53750b7ef4ee441b16e86b1ba81ff14e07536
lt468 <lucatremblayuk@gmail.com>
2023-08-09 15:48:40 -0700

    fixing forms

--- auctions/models.py
+++ auctions/models.py
@@ -1,4 +1,4 @@
-from django.contrib.auth.models import AbstractUser - cons
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 def user_media_path(instance, fname):
@@ -17,28 +17,33 @@ def user_media_path(instance, fname):
 class User(AbstractUser):
     pass
 
-class Listings(models.Model):
+class Listing(models.Model):
     item_id = models.AutoField(primary_key=True)
     item_title = models.CharField(max_length=64)
     description = models.TextField()
     # There must always be a starting bid price
-    starting_bid = models.DecimalField(max_digits=None, decimal_places=2, default=0.01)
-    current_bid = models.DecimalField(max_digits=None, decimal_places=2, default=0.01)
+    starting_bid = models.DecimalField(max_digits=10, decimal_places=2, default=0.01)
+    current_bid = models.DecimalField(max_digits=10, decimal_places=2, default=0.01)
     category = models.CharField(max_length=64)
     item_image = models.ImageField(upload_to=user_media_path)
     owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name="lister")
 
 class Bid(models.Model):
     bid_id = models.AutoField(primary_key=True)
-    item = models.ForeignKey(Listings, on_delete=models.CASCADE, related_name="bids")
-    bidder = models.ForeignKey(User, on_delete=models.CASCADE, related_name="lister")
-    bid_amount = models.DecimalField(max_digits=None, decimal_places=2, default=0.01)
+    item = models.ForeignKey(Listing, on_delete=models.CASCADE, related_name="bids")
+    bidder = models.ForeignKey(User, on_delete=models.CASCADE, related_name="item_lister")
+    bid_amount = models.DecimalField(max_digits=10, decimal_places=2, default=0.01)
     bid_time = models.DateTimeField(auto_now_add=True)
 
-class Comments(models.Model):
+class Comment(models.Model):
     comment_id = models.AutoField(primary_key=True)
     comment_text = models.TextField()
     # When a commenter, a user, is deleted then all their comments are deleted too
     commenter = models.ForeignKey(User, on_delete=models.CASCADE, related_name="buyer")
     comment_time = models.DateTimeField(auto_now_add=True)
+
+class WatchList(models.Model):
+    watch_id = models.AutoField(primary_key=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    item = models.ForeignKey(Listing, on_delete=models.CASCADE)
 
